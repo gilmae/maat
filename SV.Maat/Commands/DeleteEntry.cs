3540bbc62c5a9c0ccae19efc32682cf63cd2d8f4
gilmae <avocadia@fastmail.fm>
2020-03-30 14:37:24 +1100

    Add event and command

---
+++ SV.Maat/Commands/DeleteEntry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Events;
+using StrangeVanilla.Blogging.Events;
+using StrangeVanilla.Blogging.Events.Entries.Events;
+using SV.Maat.lib;
+
+namespace SV.Maat.Commands
+{
+    public class DeleteEntry : BaseCommand<Entry>
+    {
+        IEventStore<Entry> _entryStore;
+        public Entry Entry { get; set; }
+
+        public DeleteEntry(IEventStore<Entry> entryStore)
+        {
+            _entryStore = entryStore;
+        }
+
+        public override Entry Execute()
+        {
+            Incrementor version = new Incrementor(_entryStore.GetCurrentVersion(Entry.Id));
+
+            var e = new EntryDeleted(Entry.Id) { Version = version.Next() };
+
+            _entryStore.StoreEvent(e);
+
+            e.Apply(Entry);
+
+            return Entry;
+        }
+    }
+}
---
+++ StrangeVanilla.Blogging.Events/Entries/Events/EntryDeleted.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Events;
+
+namespace StrangeVanilla.Blogging.Events.Entries.Events
+{
+    public class EntryDeleted : Event<Entry>
+    {
+        public EntryDeleted()
+        {
+        }
+
+        public DateTime DeletedAt { get; set; }
+
+        public EntryDeleted(Guid entryId)
+        {
+            AggregateId = entryId;
+            DeletedAt = DateTime.UtcNow;
+        }
+
+        public override void Apply(Entry aggregate)
+        {
+            base.Apply(aggregate);
+            aggregate.DeletedAt = DeletedAt;
+        }
+    }
+}
