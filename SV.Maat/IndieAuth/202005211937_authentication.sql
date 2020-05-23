9f26e710a58a556ed40c4faa2cc55d140de98c8e
gilmae <avocadia@fastmail.fm>
2020-05-23 18:26:21 +1000

    Rename the database setup files
    Say, to ensure they can be copied into a single folder

---
+++ SV.Maat/IndieAuth/202005211937_authentication.sql
@@ -0,0 +1,52 @@
+﻿-- Table: public.authenticationrequests
+
+-- DROP TABLE public.authenticationrequests;
+
+CREATE TABLE public.authenticationrequests
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    "UserProfileUrl" character varying(128) COLLATE pg_catalog."default",
+    "ClientId" character varying(128) COLLATE pg_catalog."default" NOT NULL,
+    "RedirectUri" character varying(256) COLLATE pg_catalog."default",
+    "CsrfToken" character varying(256) COLLATE pg_catalog."default",
+    "ResponseType" character varying(128) COLLATE pg_catalog."default" NOT NULL,
+    "AuthorisationCode" character varying(128) COLLATE pg_catalog."default",
+    "ClientName" character varying(128) COLLATE pg_catalog."default",
+    "ClientLogo" character varying(128) COLLATE pg_catalog."default",
+    "Scope"  character varying(128) COLLATE pg_catalog."default",
+    "AuthCodeExpiresAt" timestamp with time zone,
+    "State" character varying(128) COLLATE pg_catalog."default",
+    "AccessToken" character varying(128) COLLATE pg_catalog."default",
+    "UserId" integer
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.authenticationrequests
+    OWNER to gilmae;
+
+
+-- Table: public.accesstokens
+
+-- DROP TABLE public.accesstokens;
+
+CREATE TABLE public.accesstokens
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    "AuthenticationRequestId" integer NOT NULL,
+    "UserId" integer NOT NULL,
+    "Name" character varying(128) COLLATE pg_catalog."default" NOT NULL,
+    "ClientId" character varying(128) COLLATE pg_catalog."default" NOT NULL,
+    "Scope" character varying(128) COLLATE pg_catalog."default" NOT NULL,
+    "Salt"  character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT accesstokens_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.accesstokens
+    OWNER to gilmae;
---
+++ SV.Maat/Syndications/202005091815_syndications.sql
@@ -0,0 +1,18 @@
+﻿-- Table: public."Syndications"
+
+-- DROP TABLE public.UserSyndicationss;
+
+CREATE TABLE public.Syndications
+(
+    Id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    "AccountName" character varying(128) COLLATE pg_catalog."default",
+    "Network" character varying(128) COLLATE pg_catalog."default" NOT NULL,
+    "UserId" int
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.Syndications
+    OWNER to gilmae;
