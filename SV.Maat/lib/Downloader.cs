c1924c4bc2b463071141d7a1c202e862a800d13e
gilmae <avocadia@fastmail.fm>
2020-06-13 16:31:14 +1000

    Add helper for downloading data from urls

---
+++ SV.Maat/lib/Downloader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SV.Maat.lib
+{
+    public static class Downloader
+    {
+        public static async Task<byte[]> Download(string url)
+        {
+            var request = WebRequest.CreateHttp(url);
+            request.AllowAutoRedirect = true;
+            using (HttpWebResponse response = await request.GetResponseAsync() as HttpWebResponse)
+            {
+                if (response == null || response.StatusCode != HttpStatusCode.OK)
+                {
+                    return null;
+                }
+
+                byte[] data = new byte[response.ContentLength];
+                var stream = response.GetResponseStream();
+
+                for (int ii = 0; ii < response.ContentLength; ii += 1000)
+                {
+                    stream.Read(data, ii, 1000);
+                }
+
+                return data;
+            }
+
+
+        }
+    }
+}
