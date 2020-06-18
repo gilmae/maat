dbeed6473c1d502fa9931ff3357ed9623e339bf4
gilmae <avocadia@fastmail.fm>
2020-06-18 19:48:20 +1000

    Finish the pinboard.in implementation

---
+++ SV.Maat/ExternalNetworks/Pinboard.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using StrangeVanilla.Blogging.Events;
+using pinboard.net;
+using pinboard.net.Models;
+
+namespace SV.Maat.ExternalNetworks
+{
+    public class Pinboard : ISyndicationNetwork, IRequiresCredentialEntry
+    {
+        public Pinboard()
+        {
+        }
+
+        public string Name => "Pinboard";
+
+        public string Photo => "https://pinboard.in/bluepin.gif";
+
+        public string Url => "https://pinboard.in";
+
+        public string Syndicate(Credentials credentials, Entry entry)
+        {
+            using (var pb = new PinboardAPI($"{credentials.Uid}:{credentials.Secret}"))
+            {
+                Bookmark bookmark = new Bookmark();
+                bookmark.Url = entry.BookmarkOf;
+                bookmark.Description = entry.Title;
+                bookmark.Extended = entry.Body;
+                bookmark.Tags.AddRange(entry.Categories);
+
+                var result = pb.Posts.Add(bookmark).Result;
+
+                if (result.ResultCode)
+                {
+                    return $"pinboard:{System.Web.HttpUtility.UrlEncode(entry.BookmarkOf)}";
+                }
+                return "";
+            }
+        }
+    }
+}
