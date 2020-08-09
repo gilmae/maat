9bba8cdbdb4f9179d78c361f0c10e7722df376f1
gilmae <avocadia@fastmail.fm>
2020-08-09 13:55:16 +1000

    Add JosnPropertyName attributes

--- SV.Maat/Micropub/Models/Config.cs
+++ SV.Maat/Micropub/Models/Config.cs
@@ -1,16 +1,20 @@
-﻿using Newtonsoft.Json;
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace SV.Maat.Micropub.Models
 {
     public class Config
     {
         [JsonProperty("media-endpoint")]
+        [JsonPropertyName("media-endpoint")]
         public string MediaEndpoint { get; set; }
 
         [JsonProperty("q")]
+        [JsonPropertyName("media-endpoint")]
         public string[] SupportedQueries { get; set; }
 
         [JsonProperty("syndicate_to")]
+        [JsonPropertyName("media-endpoint")]
         public object[] SupportedSyndicationNetworks { get; set; }
 
     }
