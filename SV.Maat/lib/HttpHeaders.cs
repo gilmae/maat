915d1bb89f00d3febe67af7df10c88961a705bdc
gilmae <avocadia@fastmail.fm>
2020-08-26 18:52:18 +1000

    Add helper for parsing the Http Link Header

---
+++ SV.Maat/lib/HttpHeaders.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace SV.Maat.lib
+{
+    public class HttpHeaders
+    {
+        const string paramsRegexPattern = "(\\w+)=['\"]?([^\"']+)['\"]?";
+        const string urlRegex = "(?<=<).+?(?=>)";
+
+        public static IEnumerable<HttpLink> ParseHttpLinkHeader(string link)
+        {
+            return link.Split(',').Select(l =>
+                new HttpLink
+                {
+                    Url = Regex.Match(l, urlRegex)?.Groups[0].Value,
+                    Params = Regex.Matches(l, paramsRegexPattern)
+                        .SelectMany(m=>
+                            m.Captures
+                                .Where(c => (c as Match).Groups.Count == 3)
+                        ).ToDictionary(
+                            c => (c as Match).Groups[1].Value,
+                            c => (c as Match).Groups[2].Value
+                   )
+                })
+                .Where(l=>!string.IsNullOrEmpty(l.Url));
+        }
+
+        public class HttpLink
+        {
+            public string Url { get; set; }
+            public Dictionary<string,string> Params { get; set; }
+        }
+    }
+}
---
+++ Tests/ParseHttpLinkHeader.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using static SV.Maat.lib.HttpHeaders;
+
+namespace Tests
+{
+    public class ParseHttpLinkHeader
+    {
+        [Fact]
+        public void HeaderWithSingleLinkReturnsOneLink()
+        {
+            var result = SV.Maat.lib.HttpHeaders.ParseHttpLinkHeader(@"<https://example.com>; rel='preconnect'");
+            var usableResult = new List<HttpLink>(result);
+
+            Assert.Single(result);
+            Assert.Equal("https://example.com", usableResult[0].Url);
+            Assert.Single(usableResult[0].Params);
+            Assert.True(usableResult[0].Params.ContainsKey("rel"));
+            Assert.Equal("preconnect", usableResult[0].Params["rel"]);
+        }
+
+        [Fact]
+        public void HeaderWithTwoLinkReturnsTwoLinks()
+        {
+            var result = SV.Maat.lib.HttpHeaders.ParseHttpLinkHeader(@"<https://example.com>; rel='preconnect',<https://two.example.com>; rel='unconnect'");
+            var usableResult = new List<HttpLink>(result);
+            Assert.Equal("https://example.com", usableResult[0].Url);
+            Assert.Single(usableResult[0].Params);
+            Assert.True(usableResult[0].Params.ContainsKey("rel"));
+            Assert.Equal("preconnect", usableResult[0].Params["rel"]);
+
+            Assert.Equal("https://two.example.com", usableResult[1].Url);
+            Assert.Single(usableResult[1].Params);
+            Assert.True(usableResult[1].Params.ContainsKey("rel"));
+            Assert.Equal("unconnect", usableResult[1].Params["rel"]);
+        }
+
+        [Fact]
+        public void HeaderWithMultipleParamsLinkReturnsOneLinkWithMultipleParams()
+        {
+            var result = SV.Maat.lib.HttpHeaders.ParseHttpLinkHeader(@"<https://example.com>; rel='preconnect'; media='print'");
+
+            Assert.Single(result);
+
+            var usableResult = new List<HttpLink> ( result );
+            Assert.Equal("https://example.com", usableResult[0].Url);
+            Assert.Equal(2, usableResult[0].Params.Count);
+            Assert.True(usableResult[0].Params.ContainsKey("rel"));
+            Assert.Equal("preconnect", usableResult[0].Params["rel"]);
+            Assert.True(usableResult[0].Params.ContainsKey("media"));
+            Assert.Equal("print", usableResult[0].Params["media"]);
+        }
+
+        [Fact]
+        public void HeaderWithInvalidlyEscapedUrlReturnsEmpty()
+        {
+            var result = SV.Maat.lib.HttpHeaders.ParseHttpLinkHeader(@"https://example.com; rel='preconnect'; media='print'");
+
+            Assert.Empty(result);
+        }
+    }
+}
