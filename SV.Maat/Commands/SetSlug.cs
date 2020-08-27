573a4b9150c40ee2e4308e02c4bf0d0294aeb7aa
gilmae <avocadia@fastmail.fm>
2020-08-27 22:08:51 +1000

    Add command and event to set a slug on the entry

---
+++ SV.Maat/Commands/SetSlug.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Events;
+using StrangeVanilla.Blogging.Events;
+using StrangeVanilla.Blogging.Events.Entries.Events;
+
+namespace SV.Maat.Commands
+{
+    public class SetSlug :ICommand
+    {
+        public string Slug { get; set; }
+
+        public SetSlug()
+        {
+        }
+
+        public Event GetEvent(int version)
+        {
+            return new SlugSet() { Slug = Slug, Version = version };
+        }
+
+        public bool IsValid(Aggregate aggregate)
+        {
+            return aggregate is Entry;
+        }
+    }
+}
---
+++ StrangeVanilla.Blogging.Events/Entries/Events/SlugSet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Events;
+
+namespace StrangeVanilla.Blogging.Events.Entries.Events
+{
+    public class SlugSet : Event<Entry>
+    {
+        public string Slug { get; set; }
+        public SlugSet() { }
+        public SlugSet(Guid entryId)
+        {
+            AggregateId = entryId;
+        }
+
+        public override void Apply(Entry aggregate)
+        {
+            base.Apply(aggregate);
+            aggregate.Slug = Slug;
+        }
+    }
+}
