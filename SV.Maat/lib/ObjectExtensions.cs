dd576ccd2886220605f94ba5a99503dc24309b07
gilmae <avocadia@fastmail.fm>
2020-09-05 17:05:19 +1000

    Add method for checking if an Enumerable is useful - not null and has content

--- SV.Maat/lib/ObjectExtensions.cs
+++ SV.Maat/lib/ObjectExtensions.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace SV.Maat.lib
 {
     public static class ObjectExtensions
@@ -6,6 +10,11 @@ namespace SV.Maat.lib
         public static bool IsNull(this object o)
         {
             return o == null;
+        }
+
+        public static bool HasContent<T>(this IEnumerable<T> o)
+        {
+            return o != null && o.Any();
         }
     }
 }
