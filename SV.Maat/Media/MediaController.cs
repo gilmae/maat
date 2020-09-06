3335fa8b4b42c7126ad429828eb6d6618f5adc88
gilmae <avocadia@fastmail.fm>
2020-09-06 18:32:49 +1000

    Requested sizes will be treated as a maximum. Images will be resized to specific sizes, and the size returned will be the largest size less than or equal to the requested size
    In part this is just to make sure Ma'at's cache can't be a vector of attack

--- SV.Maat/Media/MediaController.cs
+++ SV.Maat/Media/MediaController.cs
@@ -23,6 +23,7 @@ namespace SV.Maat.MediaView
         readonly IProjection<Media> _mediaProjection;
         readonly IFileStore _fileStore;
         IMemoryCache _memoryCache;
+        readonly int[] sizes = new int[] { 16, 32, 64, 128, 640, 800, 1024, 2048, 4096 };
 
         public MediaController(IProjection<Media> mediaProjection, IFileStore fileStore, IMemoryCache memoryCache)
         {
@@ -53,7 +54,16 @@ namespace SV.Maat.MediaView
             var m = _mediaProjection.Get(id);
             if (m != null)
             {
-                string cacheKey = $"{id}_{width}";
+                // Width will be considered the maximum width desired. Maat will only support a set of defined widths,
+                // so find the width we can return based less than or equal to the max width
+
+                int actualWidth = sizes.LastOrDefault(x => x <= width);
+                if (actualWidth == 0)
+                {
+                    actualWidth = sizes.First();
+                }
+
+                string cacheKey = $"{id}_{actualWidth}";
 
                 if (_memoryCache.TryGetValue<byte[]>(cacheKey, out byte[] data))
                 {
@@ -64,7 +74,7 @@ namespace SV.Maat.MediaView
 
                 using (Image image = Image.Load(data))
                 {
-                    float scale = (float)width / image.Width;
+                    float scale = (float)actualWidth / image.Width;
                     int height = (int)Math.Floor(image.Height * scale);
                     SixLabors.ImageSharp.Formats.IImageEncoder encoder = new JpegEncoder();
                     if (m.MimeType == "image/gif")
@@ -76,13 +86,13 @@ namespace SV.Maat.MediaView
                         encoder = new PngEncoder();
                     }
 
-                    image.Mutate(x => x.Resize(width, height, KnownResamplers.Lanczos3));
+                    image.Mutate(x => x.Resize(actualWidth, height, KnownResamplers.Lanczos3));
                     var ms = new MemoryStream();
                     image.Save(ms, encoder);
                     ms.Position = 0;
 
                     byte[] resizedData = new byte[ms.Length];
-                    for (int i = 0; i < ms.Length; i++)
+                    for (int i=0; i < ms.Length; i++)
                     {
                         ms.Read(resizedData, i, 1);
                     }
@@ -92,6 +102,7 @@ namespace SV.Maat.MediaView
 
                     var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(30));
 
+
                     _memoryCache.Set<byte[]>(cacheKey, resizedData, cacheEntryOptions);
                     return new FileContentResult(resizedData, m.MimeType);
                 }
@@ -99,5 +110,7 @@ namespace SV.Maat.MediaView
 
             return NotFound();
         }
+
+
     }
 }
