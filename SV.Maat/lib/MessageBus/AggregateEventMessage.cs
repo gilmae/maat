2ced3e35908968ff5306416e1136956b63639f75
gilmae <avocadia@fastmail.fm>
2020-09-12 16:58:52 +1000

    Snip out the message bus folder. We're just not using it any longer.

--- SV.Maat/lib/MessageBus/AggregateEventMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿//using System;
-//namespace SV.Maat.lib.MessageBus
-//{
-//    public class AggregateEventMessage
-//    {
-//        public Guid Id { get; set; }
-//        public int Version { get; set; }
-//    }
-//}
--- SV.Maat/lib/MessageBus/IMessageBus.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace SV.Maat.lib.MessageBus
-{
-    public interface IMessageBus
-    {
-        public void Publish(dynamic message);
-    }
-}
